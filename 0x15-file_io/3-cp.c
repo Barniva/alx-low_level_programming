c56436631f977cfcc78c0022009522a8805aa33d
Barnabas Endashaw <123116734+Barniva@users.noreply.github.com>
2023-04-15 01:03:58 +0300

    run

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -80,6 +80,15 @@ int main(int argc, char *argv[])
 			free(buffer);
 			exit(98);
 		}
+		
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
 
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
