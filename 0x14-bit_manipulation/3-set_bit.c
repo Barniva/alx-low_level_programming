e403e5cf1dba53da6e54fce40bd610d84204277c
Barniva <barnabasva1@gmail.com>
2023-05-04 16:22:47 -0700

    TB

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_Nit - sets a bit at a given index to 1
+ * @b: number to be used
+ * @insex: index to be set
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	*n = ((1UL << index) | *n);
+	return (1);
+}
